38140628b74a4a87727312d590dc56de462ace8a
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-04-28 17:11:27 +0600

    url iteratae music/ all album_title link in page by url link

--- music/views.py
+++ music/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 
+from music.models import Album
+
 def index(request):
-    return HttpResponse("<h1>This is new website</h1>")
+    all_album=Album.objects.all()
+    html=''
+    for album in all_album:
+        url='music'+ str(album.id) + '/'
+        html +=' <a href= " ' + url + ' " > ' + album.album_title + ' </a> <br> '
+    return HttpResponse(html)
+
 def details(request, album_id):
-    return HttpResponse("<h1>This is new website" +str(album_id)+"</h1>")
+    return HttpResponse("<h1>This is new website : " +str(album_id)+"</h1>")
