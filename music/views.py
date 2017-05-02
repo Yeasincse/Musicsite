867c27e42fe975ef663e6ecba3ef88b654229ea4
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-03 02:03:28 +0600

    added music app inside  templates directory than create subfolder music/index.html

--- music/views.py
+++ music/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from music.models import Album
 
 def index(request):
     all_album=Album.objects.all()
-    html=''
-    for album in all_album:
-        url='music'+ str(album.id) + '/'
-        html +=' <a href= " ' + url + ' " > ' + album.album_title + ' </a> <br> '
-    return HttpResponse(html)
+    template=loader.get_template("music/index.html")
+    context={
+        'all_album':all_album
+    }
+    return HttpResponse(template.render(context, request))
 
 def details(request, album_id):
     return HttpResponse("<h1>This is new website : " +str(album_id)+"</h1>")
