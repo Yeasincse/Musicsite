91e26d210f124e0b90ba953ba037c922e9ad4799
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-03 09:49:57 +0600

    change Httpresponse template loader added method shortcuts render method

--- music/views.py
+++ music/views.py
@@ -1,15 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from music.models import Album
 
 def index(request):
     all_album=Album.objects.all()
-    template=loader.get_template("music/index.html")
-    context={
-        'all_album':all_album
+    context={'all_album':all_album
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 def details(request, album_id):
     return HttpResponse("<h1>This is new website : " +str(album_id)+"</h1>")
