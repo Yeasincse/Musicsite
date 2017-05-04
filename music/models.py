8a7fe1ac4dfe1a57ad6c101cc55ba9438c47c715
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-04 13:31:16 +0600

    added is_favourite model as song

--- music/models.py
+++ music/models.py
@@ -14,5 +14,6 @@ class Song(models.Model):
     album=models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type=models.CharField(max_length=10)
     song_title=models.CharField(max_length=250)
+    is_favourite=models.BooleanField(default=False)
     def __str__(self):
         return self.song_title
