9117e2ee78ef192d33efe4dc4d5b92b1fad7ca18
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-04 12:29:26 +0600

    music/templates/music/index.html and music/views.py change that Removing Hardcoded URLs from index.html and Namespace and HTTP 404 Shortcut from views.py

--- music/views.py
+++ music/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-#from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from music.models import Album
 
@@ -8,14 +6,8 @@ def index(request):
     all_album=Album.objects.all()
     return render(request, 'music/index.html', { 'all_album':all_album })
 
-#def details(request, album_id):
-    #return HttpResponse("<h1>This is new website : " +str(album_id)+"</h1>")
-
 def details(request, album_id):
-    try:
-      album=Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("The Album does'not exist")
+    album=get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', { 'album': album })
 
 
