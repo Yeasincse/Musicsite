deefd04448db3d6d1052370f6f0e7292153529cb
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-08 00:58:54 +0600

    added favourite list item songlist than create bootstrap file added ,new directory file create,static,music,images,style.css, etc create that dynamic navbar design

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
+app_name='music'
 
 urlpatterns = [
     #music/
     url(r'^$', views.index, name="index"),
-    #music/123/
+    #music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.details, name="details"),
+    #music/<album_id/favourite/
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from music.models import Album
+from music.models import Album, Song
 
 def index(request):
     all_album=Album.objects.all()
@@ -9,6 +9,18 @@ def index(request):
 def details(request, album_id):
     album=get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', { 'album': album })
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album':album, 'error_message': "you did not right select song",
+                                                     })
+    else:
+        selected_song.is_favourite=True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
 
 
 
