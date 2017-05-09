32ad1dd4c2dcfce2f776829a5b2eb6351e9e70dd
Yeasin Rahaman <yeasin@Yeasins-MacBook-Pro.local>
2017-05-10 01:37:29 +0600

    Genericd view page and chage that viewa page generic and create base template

--- music/urls.py
+++ music/urls.py
@@ -4,9 +4,7 @@ app_name='music'
 
 urlpatterns = [
     #music/
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
     #music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.details, name="details"),
-    #music/<album_id/favourite/
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,26 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from  django.views import generic
+from .models import Album
 
-from music.models import Album, Song
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def index(request):
-    all_album=Album.objects.all()
-    return render(request, 'music/index.html', { 'all_album':all_album })
+    def get_queryset(self):
+        return Album.objects.all()
 
-def details(request, album_id):
-    album=get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', { 'album': album })
-
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song=album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {'album':album, 'error_message': "you did not right select song",
-                                                     })
-    else:
-        selected_song.is_favourite=True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
-
-
-
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
